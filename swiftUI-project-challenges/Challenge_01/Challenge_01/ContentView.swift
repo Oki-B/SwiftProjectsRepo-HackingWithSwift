d203357e0af76661eb1ce6b649ef19932e6fc56e
Oki <biek.syaoki@gmail.com>
2025-05-30 21:40:21 +0700

    Feat: Init - challenge 01 project

---
+++ swiftUI-project-challenges/Challenge_01/Challenge_01/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Challenge_01
+//
+//  Created by Syaoki Biek on 30/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
