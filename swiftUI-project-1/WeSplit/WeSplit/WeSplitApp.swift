5fccc6d3cd926b06e976ee560e4f47b87e108770
Oki <biek.syaoki@gmail.com>
2025-05-30 18:10:50 +0700

    Re-Init Project 1

---
+++ swiftUI-project-1/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Syaoki Biek on 30/05/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
