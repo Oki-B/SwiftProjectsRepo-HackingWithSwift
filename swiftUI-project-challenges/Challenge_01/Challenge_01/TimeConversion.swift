dea4ecdd8f37bfd73be4fe96f4cf1c96321b0e7c
Oki <biek.syaoki@gmail.com>
2025-05-31 21:16:59 +0700

    Feat: Challenge-01 Done

--- swiftUI-project-challenges/Challenge_01/Challenge_01/TimeConversion.swift
+++ swiftUI-project-challenges/Challenge_01/Challenge_01/TimeConversion.swift
@@ -8,11 +8,66 @@
 import SwiftUI
 
 struct TimeConversion: View {
+    @Binding var time: Double
+    @State private var selectedUnit: String = "seconds"
+    @FocusState private var isFocused: Bool
+    @State private var selectedConversionUnit: String = "seconds"
+
+    let units: [String: UnitDuration] = [
+        "seconds": .seconds, "minutes": .minutes, "hours": .hours,
+    ]
+
+    var valueToConvert: Measurement<UnitDuration> {
+        .init(value: time, unit: units[selectedUnit] ?? .seconds)
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                Section("Input time") {
+                    TextField("Time", value: $time, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($isFocused)
+
+                    Picker("Unit of time", selection: $selectedUnit) {
+                        ForEach(units.keys.sorted(), id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.menu)
+
+                }
+
+                Section("Converted Time") {
+                    Picker("Unit of time", selection: $selectedConversionUnit) {
+                        ForEach(units.keys.sorted(), id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.palette)
+
+                    if let unit = units[selectedConversionUnit] {
+                        let convertedValue = valueToConvert.converted(to: unit)
+                            .value
+
+                        Text("\(convertedValue.formatted()) \(unit.symbol)")
+                    }
+                }
+
+            }
+
+            .toolbar {
+                if isFocused {
+                    Button("Done") {
+                        isFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    TimeConversion()
+    @Previewable @State var time: Double = 0
+    TimeConversion(time: $time)
 }
--- swiftUI-project-challenges/Challenge_01/Challenge_01/VolumeConversion.swift
+++ swiftUI-project-challenges/Challenge_01/Challenge_01/VolumeConversion.swift
@@ -8,11 +8,59 @@
 import SwiftUI
 
 struct VolumeConversion: View {
+    @Binding var volume: Double
+    @State private var selectedUnit: String = "cups"
+    @State private var selectedConversionUnits = "cups"
+    @FocusState private var isFocused: Bool
+    
+    let units: [String: UnitVolume] = ["milliliters": .milliliters, "liters": .liters, "cups": .cups, "pints": .pints, "gallons": .gallons]
+    
+    private var valueToConvert: Measurement<UnitVolume> {
+        .init(value: volume, unit: units[selectedUnit] ?? .cups)
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                Section("Input Volume") {
+                    TextField("Volume", value: $volume, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($isFocused)
+                    
+                    Picker("Unit of volume", selection: $selectedUnit) {
+                        ForEach(units.keys.sorted(), id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.automatic)
+                }
+                
+                Section("Converted Volume") {
+                    Picker("Pick conversion unit", selection: $selectedConversionUnits) {
+                        ForEach(units.keys.sorted(), id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    
+                    if let unit = units[selectedConversionUnits] {
+                        let convertedValue = valueToConvert.converted(to: unit).value
+                        Text("\(convertedValue.formatted()) \(unit.symbol)")
+                    }
+                }
+            }
+            .toolbar {
+                if isFocused {
+                    Button("Done") {
+                        isFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    VolumeConversion()
+    @Previewable @State var volume: Double = 100
+    VolumeConversion(volume: $volume)
 }
