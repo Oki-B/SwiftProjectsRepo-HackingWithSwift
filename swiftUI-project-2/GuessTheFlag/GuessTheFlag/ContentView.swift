c98579fceaaf9b859196b0f9d618f8a4275397b2
Oki <biek.syaoki@gmail.com>
2025-05-31 23:43:30 +0700

    feat: using stack to arrange views

--- swiftUI-project-2/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ swiftUI-project-2/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,10 +10,23 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer ()
+            VStack(alignment: .leading, spacing: 20) { // create a stack vertically
+                Text("Hello, world!")
+                Text("This is another text view")
+            }
+            
+            Spacer() // fill the empty space
+            HStack (spacing: 20) { // create a stack horizontally
+                Text("Hello")
+                Text("World")
+            }
+            Spacer ()
+            ZStack(alignment: .top) { // create a stack from back to the front
+                Text("Back text")
+                Text("Front of the text")
+            }
+                Spacer()
         }
         .padding()
     }
