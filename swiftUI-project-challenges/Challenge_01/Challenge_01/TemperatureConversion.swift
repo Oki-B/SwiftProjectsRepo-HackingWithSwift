88d54e0ee6960ec7ff736e4dd3a7f3c406816a48
Oki <biek.syaoki@gmail.com>
2025-05-31 21:14:15 +0700

    feat: Challenge 01 Project Done

---
+++ swiftUI-project-challenges/Challenge_01/Challenge_01/TemperatureConversion.swift
@@ -0,0 +1,18 @@
+//
+//  TemperatureConversion.swift
+//  Challenge_01
+//
+//  Created by Syaoki Biek on 31/05/25.
+//
+
+import SwiftUI
+
+struct TemperatureConversion: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TemperatureConversion()
+}
---
+++ swiftUI-project-challenges/Challenge_01/Challenge_01/TimeConversion.swift
@@ -0,0 +1,18 @@
+//
+//  TimeConversion.swift
+//  Challenge_01
+//
+//  Created by Syaoki Biek on 31/05/25.
+//
+
+import SwiftUI
+
+struct TimeConversion: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimeConversion()
+}
