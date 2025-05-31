1d722cfb5f0b397646f0284155c9450d657f4ed8
Oki <biek.syaoki@gmail.com>
2025-06-01 02:05:45 +0700

    feat : Warp up challenge Project-2

--- swiftUI-project-2/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ swiftUI-project-2/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -7,15 +7,27 @@
 
 import SwiftUI
 
+/*
+ Challenge :
+    1. Add an @State property to store the user’s score, modify it when they get an answer right or wrong, then display it in the alert and in the score label.
+    2. When someone chooses the wrong flag, tell them their mistake in your alert message – something like “Wrong! That’s the flag of France,” for example.
+    3. Make the game show only 8 questions, at which point they see a final alert judging their score and can restart the game.
+
+ */
+
 struct ContentView: View {
     @State private var countries = [
         "Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Monaco",
         "Poland", "Russia", "Spain", "UK", "US",
     ].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var userAnswer = 0  // for challenge number 2
 
     @State private var showingScore = false
     @State private var scoreTitle = ""
+
+    @State private var userScore = 0  // challenge number 1
+    @State private var questionNumber = 1 // for challenge number 3
 
     var body: some View {
         ZStack {
@@ -38,11 +50,11 @@ struct ContentView: View {
 
             VStack {
                 Spacer()
-                
+
                 Text("Guess the Flag")
                     .font(.largeTitle.bold())
                     .foregroundStyle(.white)
-                
+
                 VStack(spacing: 15) {
                     VStack {
                         Text("Tap the flag of")
@@ -52,7 +64,6 @@ struct ContentView: View {
                         Text(countries[correctAnswer])
                             .font(.largeTitle.weight(.semibold))
                     }
-
 
                     ForEach(0..<3) { number in
                         Button {
@@ -71,35 +82,58 @@ struct ContentView: View {
                 .clipShape(.rect(cornerRadius: 20))
                 Spacer()
                 Spacer()
-                
-                Text("Score: ???")
+
+                Text("Score: \(userScore)")  // challenge number 1
                     .font(.title.bold())
                     .foregroundStyle(.white)
-                
+
                 Spacer()
             }
             .padding()
 
         }
         .alert(scoreTitle, isPresented: $showingScore) {
-            Button("Continue", action: askQuestion)
-        } message: {
-            Text("Your score is: ??? ")
-        }
+            if questionNumber < 8 {
+                Button("Continue", action: askQuestion)
+            } else { // challenge number 3
+                Button("Play Again", action: resetGame)
+            }
 
+        } message: {
+            if questionNumber < 8 {
+                if scoreTitle == "Wrong" {
+                    Text("That is the flag of \(countries[userAnswer])")  // challenge number 2
+                } else {
+                    Text("Your score is: \(userScore)")  // challenge number 1
+                }
+            } else { // challenge number 3
+                Text("Your final score is: \(userScore)")
+            }
+
+        }
+    }
+
+    func resetGame() { // challenge number 3
+        questionNumber = 0
+        userScore = 0
+        askQuestion()
     }
 
     func flagTapped(_ number: Int) {
-        if number == correctAnswer {
-            scoreTitle = "Correct"
-        } else {
-            scoreTitle = "Wrong"
+        userAnswer = number
+        if questionNumber <= 8 { // challenge number 3
+            if number == correctAnswer {
+                scoreTitle = "Correct"
+                userScore += 1  // challenge number 1
+            } else {
+                scoreTitle = "Wrong"
+            }
         }
-
         showingScore = true
     }
 
     func askQuestion() {
+        questionNumber += 1
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
