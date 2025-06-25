4a71d85e693bbfc79d3ab462564a5c9549694558
Oki <biek.syaoki@gmail.com>
2025-06-26 00:43:10 +0700

    feat: Running code when apps launches

--- swiftUI-project-04/WordScramble/WordScramble/ContentView.swift
+++ swiftUI-project-04/WordScramble/WordScramble/ContentView.swift
@@ -8,15 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWord = [String]()
+    @State private var rootWord: String = ""
+    @State private var newWord: String = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+        NavigationStack {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)  // to prevent auto capitalize on textfield
+                }
+
+                Section {
+                    ForEach(usedWord, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")  // utilize sf symbol to make character count state
+                            Text(word)
+                        }
+
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
         }
-        .padding()
     }
+
+    func addNewWord() {
+        let word = newWord.lowercased().trimmingCharacters(
+            in: .whitespacesAndNewlines
+        )
+
+        guard word.count > 0 else {
+            return
+        }
+
+        // extra validation
+        withAnimation {  // add simple animation for element
+            usedWord.insert(word, at: 0)  // insert new word at first index
+        }
+        newWord = ""  // reset new word
+
+    }
+    
+    func startGame() {
+        // try to load file from bundle
+        if let startWordURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            // try to get content from file
+            if let startWords = try? String(contentsOf: startWordURL, encoding: .utf8) {
+                // seperate all words from content
+                let allWords = startWords.components(separatedBy: "\n")
+                
+                // create random element from the separated word
+                rootWord = allWords.randomElement() ?? "Asimelekete"
+                
+                // return all the load
+                return
+            }
+        }
+        
+        // if we get here, then the apps failed to load. use Fatal error to trigger and error report
+        fatalError("Could not load start.txt from bundle")
+    }
+
 }
 
 #Preview {
