121d2928e5664bb3602a950c84877f8ee28a8597
Oki <biek.syaoki@gmail.com>
2025-06-26 14:46:38 +0700

    feat: Customizing animations in SwiftUI

--- swiftUI-project-04/Animations/Animations/ContentView.swift
+++ swiftUI-project-04/Animations/Animations/ContentView.swift
@@ -9,18 +9,152 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var animationAmount = 1.0
-    
+    @State private var animationAmountOnAppear = 1.0
+
     var body: some View {
-        Button("Tap Me") {
-            animationAmount += 1
+        VStack {
+            Button("Tap Me") {
+                animationAmount += 0.5
+            }
+            .buttonStyle(.borderedProminent)
+            .padding(.top, 50)
+
+            Spacer()
+
+            VStack(spacing: 50) {
+                HStack(spacing: 50) {
+                    VStack(spacing: 50) {
+                        Circle()
+                            .frame(width: 20, height: 20)
+                            .padding(10)
+                            .background(.red)
+                            .foregroundStyle(.white)
+                            .clipShape(.circle)
+                            .scaleEffect(animationAmount)
+                            .blur(radius: (animationAmount - 1) * 0.5)
+                            .animation(.linear, value: animationAmount)
+                        Text("Linear")
+                    }
+
+                    VStack(spacing: 50) {
+                        Circle()
+                            .frame(width: 20, height: 20)
+                            .padding(10)
+                            .background(.red)
+                            .foregroundStyle(.white)
+                            .clipShape(.circle)
+                            .scaleEffect(animationAmount)
+                            .blur(radius: (animationAmount - 1) * 0.5)
+                            .animation(
+                                .spring(duration: 1, bounce: 0.9),
+                                value: animationAmount
+                            )
+                        Text("Spring")
+                    }
+
+                    VStack(spacing: 50) {
+                        Circle()
+                            .frame(width: 20, height: 20)
+                            .padding(10)
+                            .background(.red)
+                            .foregroundStyle(.white)
+                            .clipShape(.circle)
+                            .scaleEffect(animationAmount)
+                            .blur(radius: (animationAmount - 1) * 0.5)
+                            .animation(
+                                .easeInOut(duration: 2),
+                                value: animationAmount
+                            )
+                        Text("Ease")
+                    }
+                }
+
+                HStack(spacing: 50) {
+                    VStack(spacing: 50) {
+                        Circle()
+                            .frame(width: 20, height: 20)
+                            .padding(10)
+                            .background(.red)
+                            .foregroundStyle(.white)
+                            .clipShape(.circle)
+                            .scaleEffect(animationAmount)
+                            .blur(radius: (animationAmount - 1) * 0.5)
+                            .animation(
+                                .easeInOut(duration: 2)
+                                    .delay(1),
+                                value: animationAmount
+                            )
+                        Text("Delay")
+                    }
+
+                    VStack(spacing: 50) {
+                        Circle()
+                            .frame(width: 20, height: 20)
+                            .padding(10)
+                            .background(.red)
+                            .foregroundStyle(.white)
+                            .clipShape(.circle)
+                            .scaleEffect(animationAmount)
+                            .blur(radius: (animationAmount - 1) * 0.5)
+                            .animation(
+                                .easeInOut(duration: 2)
+                                    .repeatCount(3, autoreverses: true),
+                                value: animationAmount
+                            )
+                        Text("Repeat")
+                    }
+
+                    VStack(spacing: 50) {
+                        Circle()
+                            .frame(width: 20, height: 20)
+                            .padding(10)
+                            .background(.red)
+                            .foregroundStyle(.white)
+                            .clipShape(.circle)
+                            .scaleEffect(animationAmount)
+                            .blur(radius: (animationAmount - 1) * 0.5)
+                            .animation(
+                                .easeInOut(duration: 2)
+                                    .repeatForever(autoreverses: true),
+                                value: animationAmount
+                            )
+                        Text("Forever")
+                    }
+                }
+
+                VStack(spacing: 50) {
+                    Circle()
+                        .fill(.blue)
+                        .frame(width: 50, height: 50)
+                        .overlay(
+                            Circle()
+                                .stroke(.blue)
+                                .scaleEffect(animationAmountOnAppear)
+                                .opacity(2 - animationAmountOnAppear)
+                                .animation(
+                                    .easeInOut(duration: 1)
+                                        .repeatForever(autoreverses: false),
+                                    value: animationAmountOnAppear
+                                )
+                        )
+
+                    Text(
+                        """
+                        Trigger animation using on Appear
+                        Utilize repeat forever to create continue anim
+                        """
+                    ).multilineTextAlignment(.center)
+                }
+                .padding(.top, 50)
+            }
+
+            Spacer()
+            Spacer()
         }
-        .padding(50)
-        .background(.red)
-        .foregroundStyle(.white)
-        .clipShape(.circle)
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3)
-        .animation(.default, value: animationAmount)
+        .onAppear {
+            animationAmountOnAppear = 2
+        }
+
     }
 }
 
