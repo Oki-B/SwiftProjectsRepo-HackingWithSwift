edf84020d3cd64d090a8e899a7a9ca293a6664fd
Oki <biek.syaoki@gmail.com>
2025-06-26 14:04:48 +0700

    feat: Project init - Animations

---
+++ swiftUI-project-04/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Syaoki Biek on 26/06/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
