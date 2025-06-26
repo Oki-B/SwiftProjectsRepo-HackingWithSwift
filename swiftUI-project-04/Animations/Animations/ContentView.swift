2c247ce57e84b25c814a16a20ace452ca28d62b5
Oki <biek.syaoki@gmail.com>
2025-06-27 03:18:59 +0700

    feat: Building custom transitions using ViewModifier

--- swiftUI-project-04/Animations/Animations/ContentView.swift
+++ swiftUI-project-04/Animations/Animations/ContentView.swift
@@ -7,37 +7,29 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
-    @State private var isShowingRectangle: Bool = false
-
+    @State private var isShowingRect = false
     var body: some View {
-        VStack {
-            Button("Tap Me") {
-                withAnimation {
-                    isShowingRectangle.toggle()
-                }
+        ZStack {
+            if !isShowingRect {
+                Rectangle()
+                    .fill(.blue)
+                    .frame(width: 200, height: 200)
+                    .transition(.asymmetric(insertion: .reversePivot, removal: .reversePivot))
             }
-            .buttonStyle(.borderedProminent)
 
-            if isShowingRectangle {
-
+            
+            if isShowingRect {
                 Rectangle()
                     .fill(.red)
-                    .frame(width: 100, height: 100)
-                    .padding(.top, 20)
-                    .transition(.scale)  // using transition for animation
-
-                Text("scale transition")
-                    .transition(.scale)
-                    .padding(.bottom, 30)
-                
-                Rectangle()
-                    .fill(.red)
-                    .frame(width: 100, height: 100)
-                    .transition(.asymmetric(insertion: .scale, removal: .opacity)) // using asymetric to create different transtion on start and end animation
-
-                Text("asymetric transition start with scale and end with opacity")
-                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+                    .frame(width: 200, height: 200)
+                    .transition(.asymmetric(insertion: .pivot, removal: .pivot))
+            }
+        }
+        .onTapGesture {
+            withAnimation(.easeInOut(duration: 0.5)) {
+                isShowingRect.toggle()
             }
         }
     }
