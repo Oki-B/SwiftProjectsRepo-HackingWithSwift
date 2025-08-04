037ef9d353cfa42ebcfbfce130a9edf7bdab21a7
Oki <biek.syaoki@gmail.com>
2025-08-04 15:41:14 +0700

    feat: Deleting items using onDelete()

---
+++ swiftUI-project-07/iExpense/iExpense/LearningMaterials/DeletingItems.swift
@@ -0,0 +1,43 @@
+//
+//  DeletingItems.swift
+//  iExpense
+//
+//  Created by Syaoki Biek on 04/08/25.
+//
+
+import SwiftUI
+
+struct DeletingItems: View {
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+
+    var body: some View {
+        NavigationStack {
+
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                Button("Add Number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+            }
+            .toolbar {  // only works in it wrapped in a navigation stack
+                EditButton()
+            }
+        }
+    }
+
+    // create function for deleting items from the list
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
+    }
+}
+
+#Preview {
+    DeletingItems()
+}
