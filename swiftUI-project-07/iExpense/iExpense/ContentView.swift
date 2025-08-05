42266e41681743d34dd5b15c9555530a4886a0b9
Oki <biek.syaoki@gmail.com>
2025-08-05 14:26:56 +0700

    feat: Building list we can delete from

--- swiftUI-project-07/iExpense/iExpense/ContentView.swift
+++ swiftUI-project-07/iExpense/iExpense/ContentView.swift
@@ -7,16 +7,42 @@
 
 import SwiftUI
 
+struct ExpenseItems {
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expenses {
+    var items = [ExpenseItems]()
+}
+
 struct ContentView: View {
+    @State private var expenses = Expenses()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                    let expense = ExpenseItems(name: "New Expense", type: "Food", amount: 100.0)
+                    expenses.items.append(expense)
+                }
+            }
         }
-        .padding()
     }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+    
 }
 
 #Preview {
