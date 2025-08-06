4600c23b8fa87d04b3643ed118087b0012cffdae
Oki <biek.syaoki@gmail.com>
2025-08-06 13:38:48 +0700

    feat: Project initiation (Moonshot)

---
+++ swiftUI-project-08/Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Syaoki Biek on 06/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
