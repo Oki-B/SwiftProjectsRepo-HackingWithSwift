e8ebded3097b4032a0d0223f87a5594ea3d4e081
Oki <biek.syaoki@gmail.com>
2025-08-09 16:09:55 +0700

    feat: Loading a specific kind of Codable data

---
+++ swiftUI-project-08/Moonshot/Moonshot/Bundle-Decodable.swift
@@ -0,0 +1,49 @@
+//
+//  Bundle-Decodable.swift
+//  Moonshot
+//
+//  Created by Syaoki Biek on 09/08/25.
+//
+
+import Foundation
+
+extension Bundle {
+    func decode(_ file: String) -> [String: Astronaut] {
+        // look for the file inside on the bundle
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Fail to locate \(file) in bundle.")
+        }
+        
+        // load the file from the bundle
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Fail to load \(file) from bundle.")
+        }
+        
+        // decode data from the file
+        let decoder = JSONDecoder()
+        
+//        guard let loaded = try? decoder.decode([String: Astronaut].self, from: data) else {
+//            fatalError("Fail to decode \(file) from bundle.")
+//        }
+//        
+//        // return the loaded data
+//        return loaded
+        
+        // creating code for troubleshooting error more specific
+        do {
+            return try decoder.decode([String: Astronaut].self, from: data)
+        } catch DecodingError.keyNotFound(let key, let context) {
+            fatalError("Failed to decode \(file) from bundle due to missing key '\(key.stringValue)' - \(context.debugDescription)")
+        } catch DecodingError.typeMismatch(_, let context) {
+            fatalError("Failed to decode \(file) from bundle due to type mismatch - \(context.debugDescription)")
+        } catch DecodingError.valueNotFound(let type, let context) {
+            fatalError( "Failed to decode \(file) from bundle due to missing \(type) value - \(context.debugDescription)")
+        } catch DecodingError.dataCorrupted(_) {
+            fatalError("Failed to decode \(file) from bundle because it appears to be invalid JSON.")
+        } catch {
+            fatalError("Faield to decode \(file) from bundle: \(error.localizedDescription)")
+        }
+        
+        
+    }
+}
