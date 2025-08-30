518153ac7a28b3646de9f19d54fe73bbd7358e6e
Oki <biek.syaoki@gmail.com>
2025-08-30 15:27:52 +0700

    feat: Navigating to different data types using NavigationPath

--- swiftUI-project-09/Navigation/Navigation/LearningMaterials/UsingNavigationPath.swift
+++ swiftUI-project-09/Navigation/Navigation/LearningMaterials/UsingNavigationPath.swift
@@ -8,8 +8,33 @@
 import SwiftUI
 
 struct UsingNavigationPath: View {
+    @State private var path = NavigationPath()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack(path: $path) { // binding the path variable
+            List {
+                ForEach(0..<5) { i in
+                    NavigationLink("You are selected: \(i)", value: i)
+                }
+                ForEach(0..<5) { i in
+                    NavigationLink("You are selected: \(i)", value: String(i))
+                }
+            }
+            .toolbar {
+                Button("Push number 556") { // we can write the navigation path through appending any value
+                    path.append(556)
+                }
+                Button("Push Hello") {
+                    path.append("Hello")
+                }
+            }
+            .navigationDestination(for: Int.self) { selection in
+                    Text("You selected number: \(selection)")
+            }
+            .navigationDestination(for: String.self) { selection in
+                    Text("You selected string: \(selection)")
+            }
+        }
     }
 }
 
