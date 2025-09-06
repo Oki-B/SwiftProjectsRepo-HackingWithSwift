2449c71095c644ae38be76f5cc86b15ba90e2c00
Oki <biek.syaoki@gmail.com>
2025-09-06 09:57:03 +0700

    feat: init project challenge 4

---
+++ swiftUI-project-challenges/Challenge_04/Challenge_04/Challenge_04App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_04App.swift
+//  Challenge_04
+//
+//  Created by Syaoki Biek on 06/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
