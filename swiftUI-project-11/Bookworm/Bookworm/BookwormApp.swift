711c639b0129e1d4743fc33dcaa0f6c70597c2c2
Oki <biek.syaoki@gmail.com>
2025-09-10 00:18:31 +0700

    feat: Project init (Bookworm)

---
+++ swiftUI-project-11/Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Syaoki Biek on 10/09/25.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
