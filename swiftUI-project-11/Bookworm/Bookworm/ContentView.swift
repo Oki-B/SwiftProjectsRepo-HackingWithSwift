1434ab296bd42080a1bcc8ac8128fc44de2b7356
Oki <biek.syaoki@gmail.com>
2025-09-14 01:01:53 +0700

    feat: Sorting SwiftData queries using SortDescriptor and Deleting from a SwiftData query

--- swiftUI-project-11/Bookworm/Bookworm/ContentView.swift
+++ swiftUI-project-11/Bookworm/Bookworm/ContentView.swift
@@ -5,15 +5,22 @@
 //  Created by Syaoki Biek on 10/09/25.
 //
 
-import SwiftUI
 import SwiftData
+import SwiftUI
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
-    @Query var books: [Book]
-    
+
+    @Query(sort: \Book.rating, order: .reverse) var books: [Book]  // how you can sort using SortDescriptor
+
+    // SortDescriptor can also be used in array, so you can sorting items using several condition you want
+    @Query(sort: [
+        SortDescriptor(\Book.title),
+        SortDescriptor(\Book.author),
+    ]) var books2: [Book]
+
     @State private var showingAddScreen = false
-    
+
     var body: some View {
         NavigationStack {
             List {
@@ -22,32 +29,45 @@ struct ContentView: View {
                         HStack {
                             EmojiRatingView(rating: book.rating)
                                 .font(.largeTitle)
-                            
+
                             VStack(alignment: .leading) {
                                 Text(book.title)
                                     .font(.headline)
-                                
+
                                 Text(book.author)
                                     .foregroundStyle(.secondary)
                             }
                         }
                     }
                 }
+                .onDelete(perform: deleteBook)
             }
-                .navigationTitle("Bookworm")
-                .navigationDestination(for: Book.self) { book in
-                    DetailView(book: book)
+            .navigationTitle("Bookworm")
+            .navigationDestination(for: Book.self) { book in
+                DetailView(book: book)
+            }
+            .toolbar {
+                
+                ToolbarItem(placement: .topBarLeading) {
+                    EditButton()
                 }
-                .toolbar {
-                    ToolbarItem(placement: .topBarTrailing) {
-                        Button("Add Book", systemImage: "plus") {
-                            showingAddScreen.toggle()
-                        }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Add Book", systemImage: "plus") {
+                        showingAddScreen.toggle()
                     }
                 }
-                .sheet(isPresented: $showingAddScreen) {
-                    AddBookView()
-                }
+            }
+            .sheet(isPresented: $showingAddScreen) {
+                AddBookView()
+            }
+        }
+    }
+
+    func deleteBook(at offsets: IndexSet) {
+        for offset in offsets {
+            let book = books[offset]
+            modelContext.delete(book)
         }
     }
 }
