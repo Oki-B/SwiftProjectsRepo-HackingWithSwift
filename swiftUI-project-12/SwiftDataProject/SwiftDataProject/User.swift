4580bbbc46f6108151cc08edb1fb622ef83256f1
Oki <biek.syaoki@gmail.com>
2025-09-14 14:16:33 +0700

    feat: Editing SwiftData model objects

---
+++ swiftUI-project-12/SwiftDataProject/SwiftDataProject/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  SwiftDataProject
+//
+//  Created by Syaoki Biek on 14/09/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class User {
+    var name: String
+    var city: String
+    var joinDate: Date
+    
+    init(name: String, city: String, joinDate: Date) {
+        self.name = name
+        self.city = city
+        self.joinDate = joinDate
+    }
+}
