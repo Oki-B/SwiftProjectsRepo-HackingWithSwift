eeacfc18612bed11275e1e1af9332c2910fa6985
Oki <biek.syaoki@gmail.com>
2025-09-14 20:21:31 +0700

    feat: Relationships with SwiftData, SwiftUI, and @Query

--- swiftUI-project-12/SwiftDataProject/SwiftDataProject/User.swift
+++ swiftUI-project-12/SwiftDataProject/SwiftDataProject/User.swift
@@ -14,6 +14,9 @@ class User {
     var city: String
     var joinDate: Date
     
+    // using macro of Relationship make us can control the variable of Job and give it some rules as example is cascade where it will automatically delete the Job Data in SwiftData if we deleting the owner
+    @Relationship(deleteRule: .cascade) var jobs = [Job]()
+    
     init(name: String, city: String, joinDate: Date) {
         self.name = name
         self.city = city
